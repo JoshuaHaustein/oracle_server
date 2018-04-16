620f37b6f44d0c207b662246d546292c70f6896e
Isac Arnekvist <isac.arnekvist@gmail.com>
2018-04-16 07:50:10 +0200

    updated reference frame for production generator

--- python_src/feasibility_gan.py
+++ python_src/feasibility_gan.py
@@ -25,7 +25,7 @@ class FeasibilityGenerator(torch.nn.Module):
 
     def forward(self, obj):
         """
-        Expects obj: (width, height, dx, dy, dθ)
+        Expects obj: (width, height, x_, y_, θ_) in object frame
         """
         z = Variable(torch.randn(obj.size(0), self.latent_size))
         if obj.is_cuda:
@@ -46,21 +46,24 @@ class GeneratorProduction(torch.nn.Module):
         self.obj_norm = Normalization(7)
         self.rob_norminv = NormalizationInverse(4)
 
-    def forward(self, obj, obj_delta):
+    def forward(self, obj, obj_):
         """
         obj       : (width, height, mass, friction, x_obj, y_obj, θ_obj)
-        obj_delta : (dx, dy, dθ)
+        obj_ : (x', y', θ')
         """
-        # obj_delta' is the delta in object frame
-        obj_delta_ = Variable(torch.zeros(obj.size(0), 7))
+        # obj_' is the delta in object frame
+        obj_delta = Variable(torch.zeros(obj.size(0), 7))
+        x, y, θ = obj.data[0, -3:]
+        x_, y_, θ_ = obj_.data[0, -3:]
         θ = obj[:, -1]
-        obj_delta_[:, :4] = obj[:, :4]
-        obj_delta_[:, 4] = torch.cos(θ) * obj_delta[:, 0] + torch.sin(θ) * obj_delta[:, 1]
-        obj_delta_[:, 5] = -torch.sin(θ) * obj_delta[:, 0] + torch.cos(θ) * obj_delta[:, 1]
-        obj_delta_[:, 6] = obj_delta[:, -1]
-        y = self.rob_norminv(self.generator(self.obj_norm(obj_delta_)))
+        obj_delta[0, :4] = obj[0, :4]
+        obj_delta[0, 4] = torch.cos(θ) * (x_ - x) + torch.sin(θ) * (y_ - y)
+        obj_delta[0, 5] = -torch.sin(θ) * (x_ - x) + torch.cos(θ) * (y_ - y)
+        obj_delta[0, 6] = obj_[0, -1]
+        y = self.rob_norminv(self.generator(self.obj_norm(obj_delta)))
+        # generator returns: x, y, cos(θ), sin(θ)
         y[:, 2] = torch.atan2(y[:, 3], y[:, 2])
-        # invariant: y is now robot position in object frame
+        # invariant: y[:, :3] = (x, y, θ) is now robot position in object frame
         # convert to robot frame
         y_rob = Variable(torch.zeros(obj.size(0), 3))
         y_rob[:, 0] = obj[:, 4] + torch.cos(θ) * y[:, 0] - torch.sin(θ) * y[:, 1]
@@ -69,3 +72,56 @@ class GeneratorProduction(torch.nn.Module):
         y_rob[:, 2] = torch.atan2(torch.sin(θr), torch.cos(θr))
         return y_rob
 
+
+def to_object_centric(X, Y):
+    """
+    X : (obj_x, obj_y, obj_θ, width, height, mass, friction)
+    Y : (obj_x', obj_y', cos(obj_θ'), sin(obj_θ'))
+    returns (width, height, mass, friction, x', y', θ'), all in object frame
+    """
+    res = torch.zeros(X.size(0), 7)
+    if X.is_cuda:
+        res = res.cuda()
+    res[:, 0] = X[:, 3]  # width
+    res[:, 1] = X[:, 4]  # height
+    res[:, 2] = X[:, 5]  # mass
+    res[:, 3] = X[:, 6]  # friction
+    θ = X[:, 2]
+    θ_ = torch.atan2(Y[:, 3], Y[:, 2])
+    # translate object successor state
+    Y_t = Y[:, :2] - X[:, :2]
+    res[:, 4] = Y_t[:, 0] * torch.cos(θ) + Y_t[:, 1] * torch.sin(θ)
+    res[:, 5] = -Y_t[:, 0] * torch.sin(θ) + Y_t[:, 1] * torch.cos(θ)
+    res[:, 6] = torch.atan2(torch.sin(θ_ - θ), torch.cos(θ_ - θ))
+    return res
+
+
+def to_robot_centric(robot, obj):
+    """
+    Expects (in object centric frame):
+    robot : (x_robot, y_robot, θ_robot)
+    obj   : (width, height, mass, friction, x', y', θ')
+
+    Returns (in robot frame):
+    X : (obj_x, obj_y, obj_θ, width, height, mass, friction)
+    Y : (obj_x', obj_y', cos(obj_θ'), sin(obj_θ'))
+    """
+    X = Variable(torch.zeros(robot.size(0), 7))
+    Y = Variable(torch.zeros(robot.size(0), 4))
+    if robot.is_cuda:
+        X = X.cuda()
+        Y = Y.cuda()
+    θr = robot[:, 2]
+    θo = obj[:, -1]
+    X[:, 0] = -robot[:, 0] * torch.cos(θr) - robot[:, 1] * torch.sin(θr)
+    X[:, 1] = robot[:, 0] * torch.sin(θr) - robot[:, 1] * torch.cos(θr)
+    X[:, 2] = torch.atan2(torch.sin(-θr), torch.cos(-θr))
+    X[:, 3] = obj[:, 0]  # width
+    X[:, 4] = obj[:, 1]  # height
+    X[:, 5] = obj[:, 2]  # mass
+    X[:, 6] = obj[:, 3]  # friction
+    Y[:, 0] = obj[:, 4] * torch.cos(θr) + obj[:, 5] * torch.sin(θr) + X[:, 0]
+    Y[:, 1] = -obj[:, 4] * torch.sin(θr) + obj[:, 5] * torch.cos(θr) + X[:, 1]
+    Y[:, 2] = torch.cos(θo - θr)
+    Y[:, 3] = torch.sin(θo - θr)
+    return X, Y
